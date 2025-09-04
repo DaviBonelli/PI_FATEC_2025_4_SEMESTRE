b565ed667beb299aefc27cd153f2c08065ab6534
Melissa Beatriz Vieira <160934310+melissabvieira@users.noreply.github.com>
2025-09-04 15:20:15 -0300

    Correção login e logout

--- Front-end/login.php
+++ Front-end/login.php
@@ -1,10 +1,17 @@
 <?php
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
     if ($usuario === "adm" && $senha === "123") {
-        echo "Login bem-sucedido!";
+        
+        $_SESSION['loggedin'] = true;
+        $_SESSION['usuario'] = $usuario; 
+
+        header("Location: index.html"); 
+        exit(); 
     } else {
         echo "Usuário ou senha inválidos.";
     }
