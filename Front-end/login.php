29d190bf1647aaa3850bc562ce71699224d9a08d
Melissa Beatriz Vieira <160934310+melissabvieira@users.noreply.github.com>
2025-09-09 15:09:17 -0300

    Correção login e logout

--- Front-end/login.php
+++ Front-end/login.php
@@ -1,17 +1,34 @@
 <?php
 session_start();
 
+$conn = mysqli_connect("localhost", "root", "", "eventos");
+
+if (!$conn) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
-    $senha = $_POST['senha'];
+    $senha   = $_POST['senha'];
 
-    if ($usuario === "adm" && $senha === "123") {
-        
+    $sql = "SELECT * FROM usuarios WHERE usuario='$usuario' AND senha='$senha' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
         $_SESSION['loggedin'] = true;
-        $_SESSION['usuario'] = $usuario; 
+        $_SESSION['usuario'] = $usuario;
 
-        header("Location: index.html"); 
-        exit(); 
+        if ($usuario === "ADM") {
+            header("Location: inicial_adm.php");
+            exit();
+        } elseif ($usuario === "FUNC") {
+            header("Location: inicial_func.php");
+            exit();
+        } else {
+            header("Location: index.html");
+            exit();
+        }
+
     } else {
         echo "Usuário ou senha inválidos.";
     }
